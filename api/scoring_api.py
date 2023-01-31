5a96505c4b3991f92267933383381addaf43f4fc
ahonestla <vicozer@gmail.com>
2023-01-31 19:51:19 +0100

    [API/DASHBOARD] Send customer list

--- api/scoring_api.py
+++ api/scoring_api.py
@@ -9,20 +9,36 @@ import sklearn
 app = FastAPI()
 
 # Unpick classifier
-clf = pickle.load(open('randomforest_v1.pckl', 'rb'))
+# clf = pickle.load(open('randomforest_v1.pckl', 'rb'))
+clf = pickle.load(open('./models/randomforest_v1.pckl', 'rb'))
 
 # Get parameters
 params = clf.get_params(deep=True)
 
+# Unpick test data
+test_df = pickle.load(open('./data/processed/test_feature_engineering.pckl', 'rb'))
+print(test_df.head())
 
 @app.get('/')
-def hello():
-    return "Hello There!"
+def main():
+    return "Hello There! This is the front page of the scoring API."
 
 
 @app.get("/test")
 def test():
     return json.dumps(params)
 
+
+@app.get("/str")
+def test():
+    return {'str': 'coucou'}
+
+
+# Return the test ids
+@app.get("/ids")
+def ids():
+    return {'ids': test_df.head().index.to_list()}
+
 if __name__ == "__main__":
-    uvicorn.run("scoring_api:app", host="0.0.0.0", port=8000)
+    uvicorn.run("scoring_api:app", reload=True, host="0.0.0.0", port=8000)
+    # uvicorn.run("scoring_api:app", host="0.0.0.0", port=8000)
--- dashboard/scoring_app.py
+++ dashboard/scoring_app.py
@@ -3,22 +3,59 @@ import pandas as pd
 import sklearn
 import streamlit as st
 import requests
+import json
+import names
+
+# API url
+API_URL = "http://0.0.0.0:8000/"
+# API_URL = "https://scoringapp-api.azurewebsites.net/"
 
 # Title
 st.title('Credit scoring application')
+st.subheader("Victor BARBIER - Data Scientist - Projet 7")
+
+
+# Functions
+# Get list of SK_IDS
+@st.cache
+def get_cust_ids():
+    response = requests.get(API_URL + "ids/")
+    content = json.loads(response.content)
+    return content['ids']
+
+# Create array of random names
+@st.cache
+def create_customer_names(n):
+    return [names.get_full_name() for _ in range(n)]
+
+
+
+
+# Get customer ids
+cust_ids = get_cust_ids()
+cust_names = create_customer_names(len(cust_ids))
+print(cust_names)
 
 # Select the customer
-cust = st.selectbox("Select the customer", ("Mark", "HTTP", "HTTPS"))
+cust_select = st.sidebar.selectbox("Select the customer", cust_ids,
+                                   format_func=lambda x: str(x) + " - " + cust_names[x])
 
-if cust == "HTTP":
-    # Get data
-    response = requests.get("http://scoringapp-api.azurewebsites.net/test")
-    print(response.json())
-    st.write(response.json())
-elif cust == "HTTPS":
-    # Get data
-    response = requests.get("https://scoringapp-api.azurewebsites.net/test")
-    print(response.json())
-    st.write(response.json())
-else:
-    st.write('Select someone')
+st.sidebar.write("You selected index " + str(cust_select))
+
+# if cust == "LOCAL":
+#     # Get data
+#     response = requests.get(API_URL + "test")
+#     print(response.json())
+#     st.write(response.json())
+# elif cust == "HTTP":
+#     # Get data
+#     response = requests.get("http://scoringapp-api.azurewebsites.net/test")
+#     print(response.json())
+#     st.write(response.json())
+# elif cust == "HTTPS":
+#     # Get data
+#     response = requests.get("https://scoringapp-api.azurewebsites.net/test")
+#     print(response.json())
+#     st.write(response.json())
+# else:
+#     st.write('Select someone')
