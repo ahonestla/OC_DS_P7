c638b6b960204a2513a919cdd1a0ea1727c10683
ahonestla <vicozer@gmail.com>
2023-02-06 11:17:44 +0100

    [DASHBOARD] Add customer id results and explanation

--- dashboard/scoring_app.py
+++ dashboard/scoring_app.py
@@ -1,34 +1,44 @@
 import pickle
+import json
 import pandas as pd
 import sklearn
 import streamlit as st
+import streamlit.components.v1 as components
 import requests
-import json
 import names
 
 # API url
 API_URL = "http://0.0.0.0:8000/"
 # API_URL = "https://scoringapp-api.azurewebsites.net/"
 
+# Timeout for requests
+TIMEOUT = 5
+
 # Title
 st.title('Credit scoring application')
 st.subheader("Victor BARBIER - Data Scientist - Projet 7")
-
+st.write("--")
 
 # Functions
-# Get list of SK_IDS
 @st.cache
 def get_cust_ids():
-    response = requests.get(API_URL + "ids/")
+    """ Get list of customers ids """
+    response = requests.get(API_URL + "ids/", timeout=TIMEOUT)
     content = json.loads(response.content)
     return content['ids']
 
-# Create array of random names
+
 @st.cache
-def create_customer_names(n):
-    return [names.get_full_name() for _ in range(n)]
+def create_customer_names(cust_numbers):
+    """ Create array of random names """
+    return [names.get_full_name() for _ in range(cust_numbers)]
 
-
+@st.cache
+def get_explanation(cust_id):
+    """ Get explanation """
+    response = requests.get(API_URL + "explain/id=" + str(cust_id), timeout=TIMEOUT)
+    content = json.loads(response.content)
+    return content['explanation']
 
 
 # Get customer ids
@@ -37,10 +47,19 @@ cust_names = create_customer_names(len(cust_ids))
 print(cust_names)
 
 # Select the customer
-cust_select = st.sidebar.selectbox("Select the customer", cust_ids,
-                                   format_func=lambda x: str(x) + " - " + cust_names[x])
+cust_select_id = st.sidebar.selectbox(
+    "Select the customer",
+    cust_ids,
+    format_func=lambda x: str(x) + " - " + cust_names[x])
 
-st.sidebar.write("You selected index " + str(cust_select))
+st.sidebar.write("You selected the customer with id=" + str(cust_select_id))
+
+
+# Display explanation
+html_string = get_explanation(cust_select_id)
+components.html(html_string, height=600)
+
+st.write("--")
 
 # if cust == "LOCAL":
 #     # Get data
